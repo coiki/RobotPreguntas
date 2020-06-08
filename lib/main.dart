4f0fdbd611052678aeb790e36688504b8780e0b2
coiki <alfonso.rocha.alonso@gmail.com>
2020-06-09 00:52:23 +0900

    Updated to new sounds and images. Removed preloading of sounds to improve experience.

--- lib/main.dart
+++ lib/main.dart
@@ -69,34 +69,50 @@ class _RobotState extends State<Robot> {
     player.fixedPlayer;
 
     //Assets caching (images and mp3s)
-    precacheImage(new AssetImage('images/robot1.png'), context);
-    precacheImage(new AssetImage('images/robot2.png'), context);
-    precacheImage(new AssetImage('images/robot3.png'), context);
-    precacheImage(new AssetImage('images/robot4.png'), context);
-    precacheImage(new AssetImage('images/robot5.png'), context);
-    precacheImage(new AssetImage('images/robot6.png'), context);
-    precacheImage(new AssetImage('images/robot7.png'), context);
-    precacheImage(new AssetImage('images/robot8.png'), context);
-    precacheImage(new AssetImage('images/robot9.png'), context);
-    player.load("audios/sonido1.mp3");
-    player.load("audios/sonido2.mp3");
-    player.load("audios/sonido3.mp3");
-    player.load("audios/sonido4.mp3");
-    player.load("audios/sonido5.mp3");
-    player.load("audios/sonido6.mp3");
-    player.load("audios/sonido7.mp3");
+    precacheImage(new AssetImage('images/Artboard1.png'), context);
+    precacheImage(new AssetImage('images/Artboard2.png'), context);
+    precacheImage(new AssetImage('images/Artboard3.png'), context);
+    precacheImage(new AssetImage('images/Artboard4.png'), context);
+    precacheImage(new AssetImage('images/Artboard5.png'), context);
+    precacheImage(new AssetImage('images/Artboard6.png'), context);
+    precacheImage(new AssetImage('images/Artboard7.png'), context);
+    precacheImage(new AssetImage('images/Artboard8.png'), context);
+    precacheImage(new AssetImage('images/Artboard9.png'), context);
+    precacheImage(new AssetImage('images/Artboard10.png'), context);
+    precacheImage(new AssetImage('images/Artboard11.png'), context);
+    precacheImage(new AssetImage('images/Artboard12.png'), context);
+    precacheImage(new AssetImage('images/Artboard13.png'), context);
+    precacheImage(new AssetImage('images/Artboard14.png'), context);
+    precacheImage(new AssetImage('images/Artboard15.png'), context);
+    precacheImage(new AssetImage('images/Artboard16.png'), context);
+    /*player.load("audios/1.mp3");
+    player.load("audios/2.mp3");
+    player.load("audios/3.mp3");
+    player.load("audios/4.mp3");
+    player.load("audios/5.mp3");
+    player.load("audios/6.mp3");
+    player.load("audios/7.mp3");
+    player.load("audios/8.mp3");
+    player.load("audios/9.mp3");
+    player.load("audios/10.mp3");
+    player.load("audios/11.mp3");
+    player.load("audios/12.mp3");
+    player.load("audios/13.mp3");
+    player.load("audios/14.mp3");
+    player.load("audios/15.mp3");
+    player.load("audios/16.mp3");*/
 
     // Returned Widget
     return Center(
       child: FlatButton(
         onPressed: () {
           setState(() {
-            robotNumber = Random().nextInt(7) + 2;
-            player.play('audios/sonido$robotNumber.mp3'); // Plays the sound
+            robotNumber = Random().nextInt(15) + 1;
+            player.play('audios/$robotNumber.mp3'); // Plays the sound
           });
         },
         child: Image.asset(
-            'images/robot$robotNumber.png'), // Displays the robot PNGs that display the message
+            'images/Artboard$robotNumber.png'), // Displays the robot PNGs that display the message
       ),
     );
   }
